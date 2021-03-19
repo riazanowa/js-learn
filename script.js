fa5d15f1678e164c8459afcde00efee542645634
Ekaterina Ryazanova <kat.riazanowa@yandex.ru>
2021-03-19 11:56:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+let number = 4.6;
+
+console.log('str' * 9);
+
+const person = `Katya`;
+
+const bool = false;
+
+
+const obj = {
+    name: "Katya",
+    age: 24,
+    isMarried: false
+};
+
+console.log(obj.age);
+
+let arr = ['plum.png', 'orange.png', 6, 'string' , []];
+
+console.log(arr[0]);
+
+const persons = `5`;
+
+const booly = false;
+
+const obj2 = {
+    name: "ALex",
+    age: 5
+};
+
+console.log(obj2.age);
+console.log(obj2["age"]);
+
+//19/03/2021
+
+alert('Hello');
+
+const result = confirm("Are you here?");
+console.log(result);
+console.log(result);
+
+
+const answer = prompt("Вам есть 18?", "18");
+console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет ?', ''); 
+
+document.write(answers);
+console.log(typeof(null));
+
+console.log(null);
+
